1fcf96806a0ea3e2bb41a21606f7571a10f3c1b5
LucasSousaAmaral <lucasdovah@gmail.com>
2025-01-17 15:30:41 -0300

    removing unecessary comments from program

--- Gertec.Storage/Program.cs
+++ Gertec.Storage/Program.cs
@@ -8,31 +8,25 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-
-//MediatR
-builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(CreateProductCommand).Assembly));
 
 // 1) Add Controllers
 builder.Services.AddControllers();
 
-// 2) Adiciona repositórios (Infrastructure)
+// 2) Add Repos
 builder.Services.AddInfrastructure(builder.Configuration);
 
-// 3) Registra o MediatR (buscando Handlers no Assembly do Commands ou de Application)
+// 3) Add MediatR
 builder.Services.AddMediatR(cfg =>
     cfg.RegisterServicesFromAssembly(typeof(CreateProductCommand).Assembly));
 
-// 4) (Opcional) Se tiver Behavior de logging de erro
+// 4) Add Middleware
 builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ErrorLoggingBehavior<,>));
 
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
